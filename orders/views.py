86df4bb6ae67926f12843ef29fe83f5b7cfb660b
Chronos4 <georgep7@outlook.com>
2019-07-22 22:14:06 +0300

    Created order model and order tasks

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .forms import OrderForm
+from .tasks import order_created
 
-# Create your views here.
+def order_create(request):
+    cart = Cart(request)
+    if request.method == "POST":
+        form = OrderForm(request.POST or None)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order,
+                                         product=item["product"],
+                                         price=item["price"],
+                                         quantity=item["quantity"])
+                # clear the cart
+                cart.clear()
+                # launch asynchronous task
+                order_created.delay(order.id)
+        return rendre(request, "orders/order/created.html", {"order": order})
+    else:
+        form = OrderForm()
+    return render(request, "orders/order/create.html", {"cart": cart, "form": form})
