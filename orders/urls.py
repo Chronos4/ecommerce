7562ac14a292640943d7e23fb1398a68f002d789
Chronos4 <georgep7@outlook.com>
2019-07-25 21:25:20 +0300

    Finalize the orders

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import order_create
+from .views import order_create, admin_order_detail
 
 
 app_name = "order"
 
 urlpatterns = [
-    path("create", order_create, name="create")
+    path("create", order_create, name="create"),
+    path("admin/order/<order_id>", admin_order_detail, name="admin_order_detail"),
+    #path("admin/order/<order_id>/pdf/",admin_order_pdf, name="admin_order_pdf")
 ]
