f7691018ae9df3eab1b874ea2677d2f63e3bbb73
Chronos4 <georgep7@outlook.com>
2019-07-25 21:26:20 +0300

    Small changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'paypal.standard.ipn',
     # my apps
     'cart',
+    'coupons',
     'orders',
     'payment',
     'shop',
@@ -139,5 +140,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join("static_cdn", "static_root")
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join("static_cdn", 'media/')
